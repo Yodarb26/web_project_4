23c6f63e3d4ba590d9ff99a6f6aa1b227a61fc99
Yodarb26 <takeshibabawaki23@icloud.com>
2021-10-28 10:13:22 +0100

    code cleaned

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const editFormBtn = document.querySelector(".profile__edit-button");
+const listTitle = document.querySelector(".profile__title");
+const listSubtitle = document.querySelector(".profile__subtitle");
+
+const modal = document.querySelector(".modal");
+
+const closeBtn = document.querySelector(".modal__popup-btn");
+
+const form = document.querySelector(".form");
+
+const titleInput = document.querySelector("#list-title");
+
+const subtitleInput = document.querySelector("#list-subtitle");
+titleInput.value = listTitle.textContent;
+subtitleInput.value = listSubtitle.textContent;
+
+function toggleModal() {
+    modal.classList.toggle("modal__open");
+}
+
+function refreshForm(event) {
+    event.preventDefault();
+    listTitle.textContent = titleInput.value;
+    listSubtitle.textContent = subtitleInput.value;
+    toggleModal();
+}
+
+form.addEventListener("submit", refreshForm, false);
+editFormBtn.addEventListener("click", toggleModal, false);
+closeBtn.addEventListener("click", toggleModal, false);
+
+
+
