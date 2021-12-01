f335cf07f83a82545ec7100c4b15868b48fb7c6c
Yodarb26 <takeshibabawaki23@icloud.com>
2021-12-01 15:31:00 +0000

    Issues: card images not fitting

--- scripts/index.js
+++ scripts/index.js
@@ -31,5 +31,55 @@ editFormBtn.addEventListener("click", openModal, false);
 closeBtn.addEventListener("click", closeModal, false);
 
 
+const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".elements-grid__cards")
 
 
+function createCard(data) {
+  const card = cardTemplate.content.querySelector(".elements-grid__card").cloneNode(true);
+  const imageElement = card.querySelector(".elements-grid__image");
+  const titleElement = card.querySelector(".elements-grid__text");
+  imageElement.src = data.url;
+  titleElement.textContent = data.title;
+  return card;
+}
+
+function addCardToPage(card){
+  cardList.prepend(card);
+}
+
+function renderCard(data){
+  addCardToPage(createCard(data));
+}
+
+const initialCards = [
+
+  {
+    title: "Yosemite Valley",
+    url: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    title: "Lake Louise",
+    url: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    title: "Bald Mountains",
+    url: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    title: "Latemar",
+    url: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    title: "Vanoise National Park",
+    url: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    title: "Lago di braies",
+    url: "https://code.s3.yandex.net/web-code/lago.jpg",
+  }
+]
+
+initialCards.forEach((cardData) => {
+  renderCard(cardData, '.elements-grid__card');
+})
