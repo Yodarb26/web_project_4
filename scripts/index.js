ecb4d9d306df3d5aaf4e4012d11179ec8c448d77
Yodarb26 <takeshibabawaki23@icloud.com>
2022-02-16 13:47:20 +0000

    fix: fixes as per reviewer feedback

--- scripts/index.js
+++ scripts/index.js
@@ -40,11 +40,10 @@ function closePopup(popup) {
   document.removeEventListener("keydown",pressEscKey);
 }
 
-//What was recommended on the feedback doesn't work for some reason if i remove profilePopup fromm its function as it doesn't open the image anymore but "edit profile" instead
-function openProfilePopup(profilePopup) {
+function openProfilePopup() {
   titleInput.value = listTitle.textContent;
   subtitleInput.value = listSubtitle.textContent;
-  openPopup(profilePopup);
+  openPopup();
 }
 
 function clickOutsideOverlay(e){
