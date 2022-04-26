97e70a6344cf338a77cee39731ca44b82449768f
Yodarb26 <takeshibabawaki23@icloud.com>
2022-04-26 18:22:28 +0100

    design: moved new js files to the components folder

---
+++ src/components/Popup.js
@@ -0,0 +1,22 @@
+class Popup {
+    constructor(popupSelector){
+        this._popupElement = document.querySelector(`.${popupSelector}`);
+    }
+
+    _handleEscUp(evt) {
+
+    }
+
+    _setEventListeners(){
+
+    }
+
+    open(){
+
+    }
+    
+    close(){
+
+    }
+}
+export default Popup;
